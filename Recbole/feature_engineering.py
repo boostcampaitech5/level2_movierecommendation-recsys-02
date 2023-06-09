ba4e9f042c5dc478a248f35f4d812fad11e3fc82
k0seo <kimyoungseo0330@gmail.com>
2023-06-09 06:53:26 +0000

    [kys] add fe

--- Recbole/feature_engineering.py
+++ Recbole/feature_engineering.py
@@ -12,6 +12,9 @@ def feature_engineering(
 ):
     
     year_data = year_preprocessing(title_data, year_data)
+    title_data = remove_year_for_title(title_data)
+    train_data = timestamp_feature(train_data)
+    writer_data,director_data,genre_data = merge_list(writer_data,director_data,genre_data)
     
     return train_data, year_data, writer_data, title_data, genre_data, director_data
 
@@ -31,3 +34,26 @@ def year_preprocessing(title_data, year_data):
     
     return year_data
 
+def timestamp_feature(train_data:pd.DataFrame)->pd.DataFrame:
+    '''
+    timestamp를 날짜 형식에 맞춰서 연산 가능한 형태로 바꿈꿈 
+    '''
+    train_data['time'] = train_data['time'].apply(lambda x: time.strftime('%Y-%m-%d-%H', time.localtime(int(x))))
+    date_df = train_data['time'].str.split("-", expand=True)
+    date_df.columns = ['ex_year', 'ex_month', 'ex_day','ex_hour'] 
+    train_data = pd.concat([train_data, date_df], axis=1)
+
+    return train_data
+
+def remove_year_for_title(title_data:pd.DataFrame)->pd.DataFrame:
+    # title에서 괄호와 괄호 내 문자열 제거
+    title_data['title'] = title_data['title'].str.replace(pat = r'\(.*\)|\s-\s.*', repl=r'', regex=True)
+    title_data['title'] = title_data['title'].str.replace(pat = r'\, The|\s-\s.*', repl=r'', regex=True)
+    title_data['title'] = title_data['title'].str.strip()
+    return title_data
+
+def merge_list(writer_data,director_data,genre_data):
+    writer_data = writer_data.groupby(by = ['item'])['writer'].apply(list).reset_index(name = 'genre')
+    director_data = director_data.groupby(by = ['item'])['director'].apply(list).reset_index(name = 'genre')
+    genre_data = genre_data.groupby(by = ['item'])['genre'].apply(list).reset_index(name = 'genre')
+    return writer_data,director_data,genre_data
