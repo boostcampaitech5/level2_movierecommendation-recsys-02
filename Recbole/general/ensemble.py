73a763e5556ae08e0d0b5acad098d0f42d94f7c8
k0seo <kimyoungseo0330@gmail.com>
2023-06-25 13:04:38 +0000

    [kys] modify gitignore

--- Recbole/general/ensemble.py
+++ Recbole/general/ensemble.py
@@ -104,74 +104,99 @@ def ensemble_weight(file_list):
 
 # # model별 랭크별 가중치를 설정하여 11-15위 추천도 활용
 # def ensemble_double_ratio():
-#     # ratio1 - Top1-10 : Top11-15
-#     print("Write ratio for Top1-10 and Top11-15. Sum of ratios must be 1.\nex) 0.75 0.25")
-#     inputs = input().strip()
-#     ratio1_list = [float(item) for item in inputs.split() if inputs]
-
-#     # exception handling
-#     if len(ratio1_list) != 2:
-#         ValueError("Wrong ratios. Try again.\n")
-#         continue
-#     if math.fabs(sum(ratio1_list) - 1.0) > sys.float_info.epsilon:
-#         ValueError("Sum of ratios is not 1. Try again.\n")
-
-#     # ratio2 - 모델별 weight 
-#     print("\nWrite ratio for input csv files. Sum of ratios must be 1.\nex) 0.5 0.5")
-#     inputs = input().strip()
-#     ratio2_list = [float(item) for item in inputs.split() if inputs]
-
-#     # exception handling
-#     if len(ratio2_list) < 2:
-#         ValueError("Wrong ratios. Try again.\n")
-#     if math.fabs(sum(ratio2_list) - 1.0) > sys.float_info.epsilon:
-#         ValueError("Sum of ratios is not 1. Try again.\n")
-
-#     # summary inputs
-#     print("\n----------input information----------")
-#     print(f"Ratio of Top1-10 & Top11-15:\n{ratio1_list[0]} : {ratio1_list[1]}")
-#     print("Ratio of input csv files:")
-#     for idx in range(len(file_list)):
-#         print(f"[{idx}] : {file_list[idx]} - {ratio2_list[idx]}")
-#     print("-------------------------------------\n")
-
-#     # get user information
-#     user_list = file_list[0]["user"].unique()
-
-
-#     # voting
-#     '''
-#     ex)
-#       model A : model B = 0.6 : 0.4
-#       top1-10 : top11-15 = 0.75 : 0.25
-
-#     1. model A & Top1-10 = 0.6 x 0.75 ~= 0.45
-#     2. model A & Top10-15 = 0.6 x 0.25 ~= 0.15
-#     3. model B & Top1-10 = 0.4 x 0.75 = 0.3
-#     4. model B & Top10-15 = 0.4 x 0.25 = 0.1
-#     '''
-#     movie_list = []
-#     idx = 0
-#     for user in tqdm(user_list, desc="Voting"):
-#         tmp_dict = dict()
-#         for i, csv in enumerate(file_list):
-#             for add in range(10):
-#                 if csv["item"][idx + add] not in tmp_dict:
-#                     tmp_dict[csv["item"][idx + add]] = ratio1_list[0] * ratio2_list[i]
-#                 else:
-#                     tmp_dict[csv["item"][idx + add]] += ratio1_list[0] * ratio2_list[i]
-#             for add in range(10, 15):
-#                 if csv["item"][idx + add] not in tmp_dict:
-#                     tmp_dict[csv["item"][idx + add]] = ratio1_list[1] * ratio2_list[i]
-#                 else:
-#                     tmp_dict[csv["item"][idx + add]] += ratio1_list[1] * ratio2_list[i]
-#         sorted_dict = sorted(tmp_dict.items(), key=lambda item: item[1], reverse=True)
-#         for i in range(10):
-#             movie_list.append(sorted_dict[i][0])
-#         idx += 15
-
+def ensemble_double_ratio(args,file_path):
     
+    # ratio1 - Top1-k : Topk-15
+    print("Write ratio for Top1-10 and Top11-15. Sum of ratios must be 1.\nex) 0.75 0.25")
+    inputs = input().strip()
+    top_weight_list = [float(item) for item in inputs.split() if inputs]
 
+    # exception handling
+    if len(top_weight_list) != 2:
+        ValueError("Wrong ratios. Try again.\n")
+    if math.fabs(sum(top_weight_list) - 1.0) > sys.float_info.epsilon:
+        ValueError("Sum of ratios is not 1. Try again.\n")
+
+
+
+    # ratio2 - 모델별 weight 
+    print("\nWrite ratio for input csv files. Sum of ratios must be 1.\nex) 0.5 0.5")
+    inputs = input().strip()
+    model_weight_list = [float(item) for item in inputs.split() if inputs]
+
+    # exception handling
+    if len(model_weight_list) < 2:
+        ValueError("Wrong ratios. Try again.\n")
+    if math.fabs(sum(model_weight_list) - 1.0) > sys.float_info.epsilon:
+        ValueError("Sum of ratios is not 1. Try again.\n")
+
+
+
+    # summary inputs
+    print("\n----------input information----------")
+    print(f"Ratio of Top1-k & Topk-15:\n{top_weight_list[0]} : {top_weight_list[1]}")
+    print("Ratio of input csv files:")
+    for idx in range(len(file_path)):
+        print(f"[{idx}] : {file_path[idx]} - {model_weight_list[idx]}")
+    print("-------------------------------------\n")
+
+
+    top1015 = pd.read_csv(file_path[0]).groupby("user").apply(lambda x: x.iloc[:10,1]).reset_index().drop('level_1', axis=1)
+    top1015.head(10)
+
+    print(top_weight_list,model_weight_list# top 1,2
+        )
+
+    file_list = []
+    for path in file_path:
+        csv = pd.read_csv(path)
+        file_list.append(csv)
+
+    user_list = file_list[0]["user"].unique()
+
+    #file_path
+
+
+
+    # voting
+    '''
+    ex)
+        model A : model B = 0.6 : 0.4
+        top1-10 : top11-15 = 0.75 : 0.25
+
+    1. model A & Top1-10 = 0.6 x 0.75 ~= 0.45
+    2. model A & Top10-15 = 0.6 x 0.25 ~= 0.15
+    3. model B & Top1-10 = 0.4 x 0.75 = 0.3
+    4. model B & Top10-15 = 0.4 x 0.25 = 0.1
+    '''
+    #user_list = file_list[0]["user"].unique()
+    file_list = []
+    for m_id, path in enumerate(file_path):
+
+        top0110 = pd.read_csv(path).groupby("user").apply(lambda x: x.iloc[:10,1]).reset_index().drop('level_1', axis=1)
+        top1115 = pd.read_csv(path).groupby("user").apply(lambda x: x.iloc[10:15,1]).reset_index().drop('level_1', axis=1)
+
+        top0110['weight'] = model_weight_list[m_id] * top_weight_list[0]
+        top1115['weight'] = model_weight_list[m_id] * top_weight_list[1]
+        file_list.append(top0110)
+        file_list.append(top1115)
+
+    #get user information
+
+
+    #print(len(file_list))
+
+    result = pd.concat(file_list).reset_index(drop=True)
+    #result[result['user']==11]
+
+
+    g = result.groupby(['user','item']).agg({'weight': 'sum'})
+    g['weight'] = - g['weight']
+    g = g.sort_values(['user','weight'])
+    g['weight'] = - g['weight']
+    result = g.groupby('user').head(10).reset_index()
+
+    return result[['user','item']]
 
 #weight
 def make_csv(args,file_list):
@@ -184,6 +209,11 @@ def make_csv(args,file_list):
     elif option == "weight":
         result,ratios = ensemble_weight(file_list)
         filename= f"{args.option} _ "+ "+".join([f"{m.split('/')[-1][:-4]}*{r}" for m,r in zip(file_list,ratios) if ".csv" in m])
+        result.to_csv(f'./output/{filename}.csv',index=False)
+
+    elif option == "db_weight":
+        result,ratios = ensemble_weight(args,file_list)
+        filename= f"{args.option} _ "+ "+".join([f"{m.split('/')[-1][:-4]}*{r}" for m,r in file_list if ".csv" in m])
         result.to_csv(f'./output/{filename}.csv',index=False)
 
 
